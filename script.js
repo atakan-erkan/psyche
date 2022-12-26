236b9e8eaf89eeaab75b96adf8898b9cea0455f3
Atakan Erkan <atakan.erkan@ogr.dpu.edu.tr>
2022-12-26 17:47:48 +0300

    Frontend

---
+++ script.js
@@ -0,0 +1,26 @@
+/* global bootstrap: false */
+(() => {
+  "use strict";
+  const tooltipTriggerList = Array.from(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach((tooltipTriggerEl) => {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
+
+function formActive() {
+  var x = document.getElementById("form-active");
+  var y = document.getElementById("carouselExampleIndicators");
+  if (x.style.display === "none" && y.style.display === "block") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function Beyaz() {
+  var element = document.body;
+  element.classList.toggle("beyaz-mode");
+}
